5aee6a9954a6055b9866edb8a6bb6016d6e05c99
PabloElectricista <pablo_spector@hotmail.com>
2024-07-15 20:29:19 -0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,73 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './views/error/Error';
+import ProtectedRoutes from './components/commons/Protected';
+
+import Home from './views/home/Home';
+import HomeMain from './components/home/homemain/HomeMain';
+import About from './components/home/about/About';
+import Contact from './components/home/contact/Contact'
+
+import Auth from './views/auth/Auth';
+import SignInPage from './components/auth/sign-in/SignIn';
+import SignUpPage  from './components/auth/sign-up/Signup';
+import Terms from './components/auth/terms/Terms';
+import Dashboard from './views/dashboard/Dashboard';
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: '/',
+          element: <HomeMain />
+        },
+        {
+          path: '/about',
+          element: <About />
+        },
+        {
+          path: '/contact',
+          element: <Contact />
+        }
+      ]
+    }, 
+    {
+      path: '/auth',
+      element: <Auth />,
+      children: [
+        {
+          path: '/auth/sign-in',
+          element: <SignInPage />
+        },
+        {
+          path: '/auth/sign-up',
+          element: <SignUpPage />
+        }, 
+        {
+          path: '/auth/term',
+          element: <Terms />
+        }
+      ]
+    },
+    {
+      element: <ProtectedRoutes />,
+      children: [
+        /*
+        RUTAS PROTEGIDAS
+        */
+      ]
+    },
+    {
+      path: '/dashboard',
+      element: <Dashboard/>
+    }
+  ])
+
+  return <RouterProvider router={router} />
+}
+
+export default App
